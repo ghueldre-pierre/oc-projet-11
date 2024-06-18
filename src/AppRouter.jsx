796bb43acc50cc0750879b47ec1a8548d82a3645
Pierre Ghueldre <ghueldre.pierre@laposte.net>
2024-06-18 14:40:15 +0200

    router uses object syntax instead of jsx

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,19 +1,23 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import { HomePage } from "./pages/HomePage";
 import { LoginPage } from "./pages/LoginPage";
 
-const AppRouter = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route index element={<HomePage />} />
-      <Route path="login" element={<LoginPage />} />
-    </Route>
-  )
-);
+const AppRouter = createBrowserRouter([
+  {
+    path:"/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
+      {
+        path: "login",
+        element: <LoginPage />
+      }
+    ]
+  }
+])
 
 export { AppRouter };
